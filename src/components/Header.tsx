141f1b5a772b7783a32c015f05365bd411931849
felipeacx <juanfe1190@gmail.com>
2025-09-30 23:18:57 -0500

    feat(header): wrap site title in Link component for navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full z-10">
       <div className="flex justify-between items-center bg-[#EEEEEE] h-16 px-16">
-        <h1 className="text-2xl font-bold text-[#585660]">GamerShop</h1>
+        <Link href="/">
+          <h1 className="text-2xl font-bold text-[#585660]">GamerShop</h1>
+        </Link>
         <nav>
           <Link href="/cart">
             <BsCart3 className="text-2xl" />
