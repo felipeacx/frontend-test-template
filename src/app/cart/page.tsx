8e34f229793bcec76cacea17eaecaf98fa874c22
felipeacx <juanfe1190@gmail.com>
2025-09-30 23:08:17 -0500

    feat(cart): create CartPage component with Header, Cart, and Footer

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer"
+import Header from "@/components/Header"
+import Cart from "./Cart"
+
+export default async function CartPage() {
+  return (
+    <>
+      <Header />
+      <Cart />
+      <Footer />
+    </>
+  )
+}
